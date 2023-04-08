6b6468c8fed182c83b0ffa81cc4f5b6a6f4532e1
Lucas Seguessi <lucas.seguessi@gmail.com>
2023-04-08 18:50:36 -0300

    Adicionado sidenav sem navegção

--- material/src/app/Material/Material.module.ts
+++ material/src/app/Material/Material.module.ts
@@ -4,6 +4,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 @NgModule({
@@ -11,13 +12,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSlideToggleModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
-  ],
-  imports: [
-    MatSlideToggleModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
-  ],
+    MatButtonModule,
+    MatSidenavModule
+  ]
 })
 export class MaterialModule { }
